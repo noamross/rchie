e36e02cfce72eb9bf3409f60bf27d4cbca38b4c6
Noam Ross <noam.ross@gmail.com>
2015-03-07 07:07:14 -0800

    Adding tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rchie)
+
+test_check("rchie")
---
+++ tests/testthat/test_match_json.R
@@ -0,0 +1,50 @@
+context('ArchieML/JSON examples match')
+
+
+test_that("ArchieML/JSON examples match", {
+	expect_identical(
+		from_archie('key: This is a value'),
+		jsonlite::fromJSON('{
+  							"key": "This is a value"
+							}'))
+
+	expect_identical(
+		from_archie('1: value
+								 2:value
+								 3   : value
+								  4:    value
+								 5:	value
+
+								 a: lowercase a
+								 A: uppercase A'),
+		jsonlite::fromJSON('{
+											   "1": "value",
+											   "2": "value",
+											   "3": "value",
+											   "4": "value",
+											   "5": "value",
+												  "a": "lowercase a",
+											   "A": "uppercase A"
+											 }'))
+
+		expect_identical(
+		from_archie('{colors.reds}
+								 crimson: #dc143c;
+								 darkred: #8b0000;
+
+								 {colors.blues}
+								 cornflowerblue: #6495ed;
+								 darkblue: #00008b;'),
+		jsonlite::fromJSON('{
+											   "colors": {
+											     "reds": {
+											       "crimson": "#dc143c;",
+											       "darkred": "#8b0000;"
+											     },
+											     "blues": {
+											       "cornflowerblue": "#6495ed;",
+											       "darkblue": "#00008b;"
+											     }
+											   }
+											 }'))
+})
