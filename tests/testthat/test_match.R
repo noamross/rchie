f6a12f886cbce73d0ccae2687d54d57835314e58
Noam Ross <noam.ross@gmail.com>
2015-03-10 12:03:04 -0700

    Fixed docx import test

--- tests/testthat/test_match.R
+++ tests/testthat/test_match.R
@@ -54,6 +54,6 @@ context('Other import formats match')
 test_that("docx imported correctly",  {
     expect_identical(
     	from_archie(system.file("ArchieMLParserTest.docx", package="rchie")),
-    	from_archie(system.file("ArchieMLParserTest.txt", package="rchie"))
+    	from_archie(system.file("ArchieMLParserTest_docx_plain.txt", package="rchie"))
     )
 	})
