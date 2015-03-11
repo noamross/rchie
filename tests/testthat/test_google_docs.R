8ffe22a2b3b5ed27841877d590cb00b7f2bcd2b6
Noam Ross <noam.ross@gmail.com>
2015-03-10 17:15:28 -0700

    Test to find testthat working directory

--- tests/testthat/test_google_docs.R
+++ tests/testthat/test_google_docs.R
@@ -1,12 +1,13 @@
 context('Online Services')
 
 test_that("google doc imported correctly",  {
+  message(getwd())
   d_token = readRDS("token_file")
   library(driver, quietly=TRUE)
   archie_test_id = '16WHsVRyCM6dHVHTvFYsTbNaIl1vavGPp8GU3OnUS7oE'
-	meta_d = file_metadata(d_token, archie_test_id)
-	tmp = tempfile()
-	download_file(d_token, meta_d, 'text/plain', tmp)
+  meta_d = file_metadata(d_token, archie_test_id)
+  tmp = tempfile()
+  download_file(d_token, meta_d, 'text/plain', tmp)
   expect_identical(
       from_archie(tmp),
       from_archie("ArchieMLParserTestFromGoogle.txt")
