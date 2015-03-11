5de582d5a17ff050c4ab295bd25f8ab7d99e1668
Noam Ross <noam.ross@gmail.com>
2015-03-10 17:31:15 -0700

    Another Travis test: listing files in test dir

--- tests/testthat/test_google_docs.R
+++ tests/testthat/test_google_docs.R
@@ -2,6 +2,7 @@ context('Online Services')
 
 test_that("google doc imported correctly",  {
   message(getwd())
+	message(paste(list.files(), "\n"))
   d_token = readRDS("token_file")
   library(driver, quietly=TRUE)
   archie_test_id = '16WHsVRyCM6dHVHTvFYsTbNaIl1vavGPp8GU3OnUS7oE'
