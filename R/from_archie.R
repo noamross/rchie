ad30a39bbabb4b186722142306ce50e6d8d7fc58
Noam Ross <noam.ross@gmail.com>
2015-03-12 10:48:25 -0700

    Consolidating markdown-specific pre-processing.

--- R/from_archie.R
+++ R/from_archie.R
@@ -6,6 +6,11 @@
 #'
 #'
 #' @param txt a string, file, or URL in ArchieML format
+#'
+#' @param markdown_extensions Should text be pre-processed so as to make
+#' markdown input compatible? Helps if text is auto-generated markdown from
+#' pandoc, and enables markdown text in keys to be preserved.
+#'
 #' @examples
 #'    from_archie(txt = 'key: value')
 #' @references \url{http://archieml.org/}
@@ -13,7 +18,7 @@
 #' @import jsonlite
 #' @importFrom tools file_ext
 #' @export
-from_archie <- function(txt) {
+from_archie <- function(txt, markdown_extensions = TRUE) {
   if (!is.character(txt)) {
     stop("Argument 'txt' must be an ArchieML string, URL or path to existing file.")
   }
@@ -33,9 +38,22 @@ from_archie <- function(txt) {
   if (length(txt) > 1) {
     txt <- paste(txt, collapse = "\n")
   }
-  txt = gsub("\n-   (?=\\w+\n)", "\n*   ", txt, perl=TRUE)
-  #Archie uses asterisks, not dashes, for lists
-  ct = new_context()
+
+
+  if(markdown_extensions) {
+
+    #protect links by double-wrapping brackets
+    txt = gsub("(\\[[^\\]]+\\])(?=\\([^\\)]+\\))","[\\1]", txt, perl=TRUE)
+    #move unescaped asterisks before a key to after
+    #stri_count_regex(txt, "(?<=(^|\\n)[\\h*]{0,100})(?<!\\\\)\\*(?=[^\\n:]*:)")
+
+    #unescape asterisks at the start of a line.
+    gsub("(^|\\n)\\h*\\\\\\*(?=\\s*\\w+)", "\n*", txt, perl=TRUE)
+    #dash bullets s as asterisks
+  	txt = gsub("\n-   (?=\\w+\n)", "\n*   ", txt, perl=TRUE)
+  }
+
+	ct = new_context()
   ct$source(system.file("archieml-js/archieml.js", package="rchie"))
   ct$assign("txt", txt)
   ct$eval("var parsed = archieml.load(txt);")
