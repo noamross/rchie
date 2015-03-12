546210c7b071b41ecfa59eeaa3d448ce441c2035
Noam Ross <noam.ross@gmail.com>
2015-03-12 14:09:09 -0700

    Draft of Shiny example app

---
+++ inst/shinyapp/server.R
@@ -0,0 +1,18 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+	output$doc = renderText({
+		input$value
+		render('archie_doc_demo.Rmd', output_format="html_document")
+		readChar("archie_doc_demo.html", file.info("archie_doc_demo.html")$size)
+
+	})
+})
