479318134c30733fdd24feb8d0d6099b9bbaab9a
Noam Ross <noam.ross@gmail.com>
2015-03-17 14:29:50 -0700

    Squashed 'inst/archieml-js/' changes from 2ad452e..241373d
    241373d Version v0.1.2
    61b2122 fixed typo in tests
    f66b69c This fixes an issue with the handling of multi-line strings in cases where a block of text that normally would be interpreted as a multi-line string also contained a "special" line (such as a scope "{}" or array "[]").
    0a7d87c Merge branch 'master' into develop
    44c1016 Merge pull request #3 from minikomi/patch-1
    5ec73ac Starting a complex or simple array in a scope that already exists as a simple key should overwrite the existing value. Fixes #6.
    daae307 Test labels seem to be the wrong way around
    96629cd Version v0.1.1
    74f4793 Use the "g" and "m" flags on the regular expression used to remove escape characters. Added tests to ensure they are all removed.
    1975bfd Add note to readme about running tests
    
    git-subtree-dir: inst/archieml-js
    git-subtree-split: 241373de5250277b2ca9a9ab08521b3226da937a

--- archieml.js
+++ archieml.js
@@ -10,10 +10,10 @@
 // which match patterns for different types of commands in AML.
 function load(input) {
   var nextLine = new RegExp('.*((\r|\n)+)');
-  var startKey = new RegExp('^\\s*([A-Za-z0-9-_\.]+)[ \t\r]*:[ \t\r]*(.*)');
-  var commandKey = new RegExp('^\\s*:[ \t\r]*(endskip|ignore|skip|end)', 'i');
-  var arrayElement = new RegExp('^\\s*\\*[ \t\r]*(.*)');
-  var scopePattern = new RegExp('^\\s*(\\[|\\{)[ \t\r]*([A-Za-z0-9-_\.]*)[ \t\r]*(?:\\]|\\})[ \t\r]*.*?(\n|\r|$)');
+  var startKey = new RegExp('^\\s*([A-Za-z0-9-_\.]+)[ \t\r]*:[ \t\r]*(.*(?:\n|\r|$))');
+  var commandKey = new RegExp('^\\s*:[ \t\r]*(endskip|ignore|skip|end).*?(\n|\r|$)', 'i');
+  var arrayElement = new RegExp('^\\s*\\*[ \t\r]*(.*(?:\n|\r|$))');
+  var scopePattern = new RegExp('^\\s*(\\[|\\{)[ \t\r]*([A-Za-z0-9-_\.]*)[ \t\r]*(?:\\]|\\}).*?(\n|\r|$)');
 
   var data = {},
       scope = data,
@@ -129,8 +129,6 @@ function load(input) {
         isSkipping = false;
         break;
     }
-
-    flushBuffer();
   }
 
   function parseScope(scopeType, scopeKey) {
@@ -160,6 +158,7 @@ function load(input) {
 
       if (scopeType == '[') {
         array = keyScope[keyBits[keyBits.length - 1]] = keyScope[keyBits[keyBits.length - 1]] || [];
+        if (typeof array == 'string') array = keyScope[keyBits[keyBits.length - 1]] = [];
         // If we're reopening this array, set the arrayType
         if (array.length > 0) arrayType = typeof array[0] === 'string' ? 'simple' : 'complex';
 
@@ -177,7 +176,7 @@ function load(input) {
       // If we're appending to a multi-line string, escape special punctuation
       // by using a backslash at the beginning of any line.
       // Note we do not do this processing for the first line of any value.
-      value = value.replace(new RegExp('^(\\s*)\\\\'), "$1");
+      value = value.replace(new RegExp('^(\\s*)\\\\', 'gm'), "$1");
     }
 
     return value;
@@ -225,6 +224,7 @@ function load(input) {
     array = null;
     arrayType = null;
     arrayFirstKey = null;
+    bufferKey = null;
   }
 
   flushBuffer();
