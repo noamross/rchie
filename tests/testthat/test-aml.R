6e7f5ba87bf06a4b3ff8fedffcc00a3d6ee400ad
Noam Ross <noam.ross@gmail.com>
2016-03-14 20:11:50 -0400

    New tests for URL and string input

--- tests/testthat/test-aml.R
+++ tests/testthat/test-aml.R
@@ -7,3 +7,12 @@ for(file in list.files(pattern = "\\.aml")) {
   })
 }
 
+test_that('import from URL works', {
+  imported = from_archie('http://archieml.org/test/1.0/all.0.aml')
+  expect_identical(imported[-(1:2)], fromJSON(imported$result))
+})
+
+test_that('import from string works', {
+  imported = from_archie(aml = 'key: value')
+  expect_identical(imported, fromJSON("{\"key\":\"value\"}"))
+})
